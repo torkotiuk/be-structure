7d8741995be679315e32a0b0f6178edf212c081c
TAL <torkotiuk@gmail.com>
2023-07-19 03:25:14 +0200

    initial

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { DB_HOST, PORT = 5000 } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1/products', routes.products);
+
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DataBase was connected successfully');
+    app.listen(PORT, console.log(`Server started on port ${PORT}`));
+  })
+  .catch(error => console.log(error));
